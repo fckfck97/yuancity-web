78a2ab2e39258d3a18b934e85fbe5dd1bd985652
Laura Cristina Lozano Negrette <lauracristinalozanonegrette@MacBook-Pro-de-Laura.local>
2025-12-18 15:05:24 -0500

    Initial commit

--- back/core/settings.py
+++ back/core/settings.py
@@ -138,24 +138,25 @@ ASGI_APPLICATION = 'core.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  "default": {
+    "ENGINE": "django.db.backends.postgresql",
+    "NAME": os.environ.get("POSTGRES_DB"),
+    "USER": os.environ.get("POSTGRES_USER"),
+    "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+    "HOST": os.environ.get("POSTGRES_HOST"),
+    "PORT": 5432,
+  }
 }
 
-
-#DATABASES = {
-#  "default": {
-#    "ENGINE": "django.db.backends.postgresql",
-#    "NAME": os.environ.get("POSTGRES_DB"),
-#    "USER": os.environ.get("POSTGRES_USER"),
-#    "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-#    "HOST": os.environ.get("POSTGRES_HOST"),
-#    "PORT": 5432,
-#  }
-#}
 
 
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
