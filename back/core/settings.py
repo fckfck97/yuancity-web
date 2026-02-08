d223f2a90fab5395fc40533d1c7485cc7c72a16a
Laura Cristina Lozano Negrette <lauracristinalozanonegrette@MacBook-Pro-de-Laura.local>
2026-02-08 15:57:40 -0500

    db

--- back/core/settings.py
+++ back/core/settings.py
@@ -142,24 +142,30 @@ ASGI_APPLICATION = 'core.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+USE_POSTGRES = os.environ.get("USE_POSTGRES", "false").lower() in ('true', '1', 'yes')
+
+if USE_POSTGRES:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_DB"),
+            "USER": os.environ.get("POSTGRES_USER"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+            "HOST": os.environ.get("POSTGRES_HOST", "127.0.0.1"),
+            "PORT": int(os.environ.get("POSTGRES_PORT", "5432")),
+        }
     }
-}
-
-
-#DATABASES = {
-#    "default": {
-#        "ENGINE": "django.db.backends.postgresql",
-#        "NAME": os.environ.get("POSTGRES_DB"),
-#        "USER": os.environ.get("POSTGRES_USER"),
-#        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-#        "HOST": os.environ.get("POSTGRES_HOST"),
-#        "PORT": 5432,
-#    }
-#}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+            "OPTIONS": {
+                "timeout": 60,
+            },
+        }
+    }
 
 
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
